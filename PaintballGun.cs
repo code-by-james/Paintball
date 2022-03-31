7a3b34b488018c611efacd5a0d73160434751c72
James <james@marketingbyjames.co.uk>
2022-03-31 13:01:15 +0100

    Add project files.

---
+++ PaintballGun.cs
@@ -0,0 +1,69 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paintball
+{
+    class PaintballGun
+    {
+        public int MagazineSize { get; private set; } = 16;
+
+        private int balls = 0;
+        public int BallsLoaded { get; private set; }
+
+        public int Balls
+        {
+            get { return balls; }
+            set
+            {
+                if (value > 0)
+                {
+                    balls = value;
+                }
+                Reload();
+            }
+        }
+
+        public PaintballGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+
+            if (!loaded)
+            {
+                Reload();
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return (BallsLoaded == 0);
+        }
+
+
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+            {
+                BallsLoaded = MagazineSize;
+            }
+            else
+            {
+                BallsLoaded = balls;
+            }
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0)
+            {
+                return false;
+            }
+
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Paintball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun(20, 16, false);
+
+
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+
+                if (gun.IsEmpty())
+                {
+                    Console.WriteLine("WARNING! You're out of ammo!");
+                }
+
+                Console.WriteLine("SPACE to shoot, r to reload, + to add ammo, q to quit");
+
+                char key = Console.ReadKey(true).KeyChar;
+
+                switch (key)
+                {
+                    // space key
+                    case ' ':
+                        Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                        break;
+                    case 'r':
+                        gun.Reload();
+                        break;
+                    case '+':
+                        gun.Balls += gun.MagazineSize;
+                        break;
+                    case 'q':
+                        return;
+                }
+
+            }
+
+        }
+    }
+}
